b97dfb636998b67765b76a3f6ad036a5ade8122d
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-09-17 15:56:01 +0800

    adding slow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
 import Navbar from "@/components/navbar";
-
+import Image from "next/image";
+import ImageCover from "@/images/photo-1.jpg"
+import { CarouselDemo } from "@/components/carousel";
 const Home = () => {
     return (
-        <Navbar />
+        <div className="flex min-h-screen w-full flex-col">
+            <Navbar />
+
+            <div className="mt-2 sm:mt-0 h-56 flex flex-col sm:flex-row justify-start items-center">
+                <div className="text-2xl sm:text-4xl text-wrap">
+                    SD Kudangan 2 adalah, Sekolah yang membimbing siswa dengan aklak dan moral yang benar berdasarkan <span className="underline font-bold bg-green-200">Profil Pelajar Pancasila.</span>
+                </div>
+                <div className="my-5 mx-5">
+                    <Image className="rounded-sm"
+                        src={ImageCover}
+                        width={1500}
+                        height={200}
+
+                        alt="logo"
+                    />
+                </div>
+
+            </div>
+            <CarouselDemo />
+        </div>
+
+
+
     )
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     router.push('/login');
   }
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
@@ -114,7 +114,7 @@ const Navbar = () => {
             </Button >    
       </header>
 
-    </div>
+   
   )
 }
 
